bbd356b9ae95783e94187f49c884d3d1a2ae7e37
Ezu-Rasi <ezupumpum1999@gmail.com>
2026-05-13 11:25:33 +0545

    Remove duplicate improperly named Typingtestcontroller.java - use TypingTestController.java instead

--- Typing_Speed_Test/src/main/java/example/com/Typing_SpeedTest/controller/Typingtestcontroller.java
+++ Typing_Speed_Test/src/main/java/example/com/Typing_SpeedTest/controller/Typingtestcontroller.java
@@ -1,5 +1,3 @@
-package example.com.Typing_SpeedTest.controller;
-
-public class TypingTestController {
-    
-}
+# This file has been removed
+# Please use TypingTestController.java instead (proper Java naming convention)
+# See TypingTestController.java for the class definition
